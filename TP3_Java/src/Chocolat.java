7332be41deff78d933b0b1f11eeac67c3f3a1eb9
AntoinePetit <antoine-59630@hotmail.fr>
2015-11-01 13:00:22 +0100

    Update Chocolat.java
    Ajout du choix dans le main

--- TP3_Java/src/Chocolat.java
+++ TP3_Java/src/Chocolat.java
@@ -270,70 +270,246 @@ public class Chocolat {
 			System.out.println("CPU Wins !");
 		keyboard.close();
 	}
+	
+	public static void gameFr (int m, int n, int i, int j) {
+		Scanner keyboard = new Scanner(System.in);
+		int[][][][] tablet = initTablet(m, n);
+		boolean cpuTurnToPlay = true;
+		int input;
+		Chocolat choco = new Chocolat();
+		Configuration actual = choco.new Configuration(m, n, i, j, tablet[m][n][i][j]);
+
+		while (actual.value != 0) {
+			List<Configuration> possibilities = new ArrayList<Configuration>();
+
+			System.out.println("================================================");
+
+			if (cpuTurnToPlay)
+				System.out.println("***Au tour de l'ordinateur***");
+			else
+				System.out.println("***A votre tour**");
+
+			System.out.println("Configuration actuelle de la tablette : " + actual);
+			System.out.println("Toutes les possibilités :");
+
+			int cpt = 1;
+
+			for (int k = 1; k <= actual.i; k++) {
+				possibilities.add(choco.new Configuration(actual.m - k, actual.n, actual.i - k, actual.j,
+						tablet[actual.m - k][actual.n][actual.i - k][actual.j]));
+			}
+			for (int k = actual.i + 1; k < actual.m; k++) {
+				possibilities.add(choco.new Configuration(k, actual.n, actual.i, actual.j,
+						tablet[k][actual.n][actual.i][actual.j]));
+			}
+			for (int k = 1; k <= actual.j; k++) {
+				possibilities.add(choco.new Configuration(actual.m, actual.n - k, actual.i, actual.j - k,
+						tablet[actual.m][actual.n - k][actual.i][actual.j - k]));
+			}
+			for (int k = actual.j + 1; k < actual.n; k++) {
+				possibilities.add(choco.new Configuration(actual.m, k, actual.i, actual.j,
+						tablet[actual.m][k][actual.i][actual.j]));
+			}
+			for (Configuration conf : possibilities)
+				System.out.println((cpt++) + ". " + conf);
+			if (!cpuTurnToPlay) {
+				System.out.println("(Entrez la configuration que vous voulez jouer)");
+				while (true) {
+
+					System.out.print("Faites votre choix >");
+					input = keyboard.nextInt() - 1;
+					if (input >= 0 && input < possibilities.size())
+						break;
+					System.out.println(":( Mauvais choix, rééssayez.");
+				}
+
+				System.out.print("Vous avez choisis ");
+				actual = possibilities.get(input);
+				System.out.println(actual);
+			} else {
+				System.out.print("L'ordinateur a choisis ");
+				actual = getBestSucc(possibilities);
+				System.out.println(actual);
+			}
+			cpuTurnToPlay = !cpuTurnToPlay;
+		}
+
+		System.out.println("================================================");
+		if (cpuTurnToPlay)
+			System.out.println("Vous avez gagné !!");
+		else
+			System.out.println("L'ordinateur a gagné !");
+		keyboard.close();
+	}
 
 	public static void main(String args[]) throws Exception {
+		final Scanner sc = new Scanner(System.in);
+		System.out.println("Bienvenue dans le jeu du carré de la mort.");
+		System.out.println("Veuillez choisir ce que vous voulez faire :");
+		System.out.println("	1. Tester les différents algotithmes sur le calcul de valeur");
+		System.out.println("	2. Tester la récupération de position pour une seule valeur");
+		System.out.println("	3. Jouer au jeu");
+		final int choix = sc.nextInt();
+		switch (choix) {
+		case 1: {
+			System.out.println("\nVous avez choisis d'expérimenter les algorithmes.");
+			System.out.println("Veuillez saisir la longueur de la tablette :");
+			final int longueur = sc.nextInt();
+			System.out.println("Veuillez saisir la largeur de la tablette :");
+			final int largeur = sc.nextInt();
+			System.out.println("Veuillez saisir la position du carré de la mort sur la longueur (entre 0 et "
+					+ (longueur - 1) + ") :");
+			final int posx = sc.nextInt();
+			System.out.println("Veuillez saisir la position du carré de la mort sur la largeur (entre 0 et "
+					+ (largeur - 1) + ") :");
+			final int posy = sc.nextInt();
+			long deb = System.nanoTime();
+			System.out.println("****************Algorithme naif****************");
+			System.out.println("Valeur de (" + longueur + "," + largeur + "," + posx + "," + posy + ") : "
+					+ getValue_naif(longueur, largeur, posx, posy));
+			System.out.println(
+					"Le temps que l'algorithme naif a prit " + ((System.nanoTime() - deb) / 1000000000) + " secondes");
+			deb = System.nanoTime();
+			System.out.println("****************Algorithme dynamique****************");
+			System.out.println("Valeur de (" + longueur + "," + largeur + "," + posx + "," + posy + ") : "
+					+ getValue_dynamique(longueur, largeur, posx, posy));
+			System.out.println("Le temps que l'algorithme dynamique a prit " + ((System.nanoTime() - deb) / 1000000000)
+					+ " secondes");
+			break;
+		}
+		case 2: {
+			System.out.println(
+					"\nVous avez choisis de tester la récupération des points possibles pour une tablette donnée et une valeur donnée.");
+			System.out.println("Veuillez saisir la longueur de la tablette :");
+			final int longueur = sc.nextInt();
+			System.out.println("Veuillez saisir la largeur de la tablette :");
+			final int largeur = sc.nextInt();
+			System.out.println(
+					"Veuillez saisir la valeur pour laquelle vous souhaitez trouver tout les points possibles :");
+			final int value = sc.nextInt();
+			final long deb = System.nanoTime();
+			final List<Point> points = getPossiblePosition(longueur , largeur, value);
+			for (final Point result : points) {
+				System.out.println("L'un des points possible qui a la valeur " + value + " est le suivant : ("
+						+ result.getX() + ", " + result.getY() + ")");
+			}
+			System.out.println("Le temps que getPossiblePosition a prit " + ((System.nanoTime() - deb) / 1000000000)
+					+ " secondes");
+
+			break;
+		}
+		case 3: {
+			System.out.println("\nVous avez choisis de jouer au jeu.");
+			System.out.println("Veuillez saisir la longueur de la tablette :");
+			final int longueur = sc.nextInt();
+			System.out.println("Veuillez saisir la largeur de la tablette :");
+			final int largeur = sc.nextInt();
+			System.out.println("Veuillez saisir la position du carré de la mort sur la longueur (entre 0 et "
+					+ (longueur - 1) + ") :");
+			final int posx = sc.nextInt();
+			System.out.println("Veuillez saisir la position du carré de la mort sur la largeur (entre 0 et "
+					+ (largeur - 1) + ") :");
+			final int posy = sc.nextInt();
+			gameFr(longueur, largeur, posx, posy);
+			break;
+		}
+		default:
+			System.out.println("\nVotre choix est invalide.");
+		}
+		sc.close();
+		System.out.println("\nFin du programme.");
+
 		// Décommenter pour tester l'algo
-		/*
-		 * long deb, tot; tot = System.nanoTime(); System.out.println(
-		 * "****************Algorithme naif****************");
-		 * System.out.println("Valeur de (3,2,2,0) : " + getValue_naif(3, 2, 2,
-		 * 0)); System.out.println("Valeur de (3,1,2,0) : " + getValue_naif(3,
-		 * 1, 2, 0)); System.out.println("Valeur de (3,2,1,0) : " +
-		 * getValue_naif(2, 2, 1, 0)); System.out.println(
-		 * "Valeur de (3,1,1,0) : " + getValue_naif(2, 1, 1, 0));
-		 * System.out.println("Valeur de (1,2,0,1) : " + getValue_naif(1, 2, 0,
-		 * 1)); System.out.println("Valeur de (1,1,0,0) : " + getValue_naif(1,
-		 * 1, 0, 0)); deb = System.nanoTime(); System.out.println(
-		 * "Valeur de (10,7,7,3) : " + getValue_naif(10, 7, 7, 3)); System.out
-		 * .println("Le temps que (10, 7, 7, 3) a prit " + ((System.nanoTime() -
-		 * deb) / 1000000000) + " secondes"); deb = System.nanoTime();
-		 * System.out.println("Valeur de (10,7,5,3) : " + getValue_naif(10, 7,
-		 * 5, 3)); System.out .println("Le temps que (10, 7, 5, 3) a prit " +
-		 * ((System.nanoTime() - deb) / 1000000000) + " secondes");
-		 * 
-		 * System.out.println(
-		 * "****************Algorithme dynamique****************");
-		 * System.out.println("Valeur de (3,2,2,0) : " + getValue_dynamique(3,
-		 * 2, 2, 0)); System.out.println("Valeur de (3,1,2,0) : " +
-		 * getValue_dynamique(3, 1, 2, 0)); System.out.println(
-		 * "Valeur de (2,2,1,0) : " + getValue_dynamique(2, 2, 1, 0));
-		 * System.out.println("Valeur de (2,1,1,0) : " + getValue_dynamique(2,
-		 * 1, 1, 0)); System.out.println("Valeur de (1,2,0,1) : " +
-		 * getValue_dynamique(1, 2, 0, 1)); System.out.println(
-		 * "Valeur de (1,1,0,0) : " + getValue_dynamique(1, 1, 0, 0)); deb =
-		 * System.nanoTime(); System.out.println("Valeur de (10,7,7,3) : " +
-		 * getValue_dynamique(10, 7, 7, 3)); System.out .println(
-		 * "Le temps que (10, 7, 7, 3) a prit " + ((System.nanoTime() - deb) /
-		 * 1000000000) + " secondes"); deb = System.nanoTime();
-		 * System.out.println("Valeur de (10,7,5,3) : " + getValue_dynamique(10,
-		 * 7, 5, 3)); System.out .println("Le temps que (10, 7, 5, 3) a prit " +
-		 * ((System.nanoTime() - deb) / 1000000000) + " secondes"); deb =
-		 * System.nanoTime(); System.out.println("Valeur de (100,100,50,50) : "
-		 * + getValue_dynamique(100, 100, 50, 50)); System.out.println(
-		 * "Le temps que (100, 100, 50, 50) a prit " + ((System.nanoTime() -
-		 * deb) / 1000000000) + " secondes"); deb = System.nanoTime();
-		 * System.out.println("Valeur de (100,100,48,52) : " +
-		 * getValue_dynamique(100, 100, 48, 52)); System.out.println(
-		 * "Le temps que (100, 100, 48, 52) a prit " + ((System.nanoTime() -
-		 * deb) / 1000000000) + " secondes"); deb = System.nanoTime();
-		 * List<Point> points = getPossiblePosition(127, 127, 127); for (Point
-		 * result : points) { System.out.println(
-		 * "L'un des points possible qui a la valeur 127 est le suivant : (" +
-		 * result.getX() + ", " + result.getY() + ")"); } System.out.println(
-		 * "Le temps que getPossiblePosition a prit " + ((System.nanoTime() -
-		 * deb) / 1000000000) + " secondes"); System.out.println(
-		 * "Le temps que l'ensemble du programme a prit " + ((System.nanoTime()
-		 * - tot) / 1000000000) + " secondes"); System.out.println(
-		 * "Fin du programme");
-		 */
+
+		// long deb, tot;
+		// tot = System.nanoTime();
+		// System.out.println("****************Algorithme
+		// naif****************");
+		// System.out.println("Valeur de (3,2,2,0) : " + getValue_naif(3, 2, 2,
+		// 0));
+		// System.out.println("Valeur de (3,1,2,0) : " + getValue_naif(3, 1, 2,
+		// 0));
+		// System.out.println("Valeur de (3,2,1,0) : " + getValue_naif(2, 2, 1,
+		// 0));
+		// System.out.println("Valeur de (3,1,1,0) : " + getValue_naif(2, 1, 1,
+		// 0));
+		// System.out.println("Valeur de (1,2,0,1) : " + getValue_naif(1, 2, 0,
+		// 1));
+		// System.out.println("Valeur de (1,1,0,0) : " + getValue_naif(1, 1, 0,
+		// 0));
+		// deb = System.nanoTime();
+		// System.out.println("Valeur de (10,7,7,3) : " + getValue_naif(10, 7,
+		// 7, 3));
+		// System.out
+		// .println("Le temps que (10, 7, 7, 3) a prit " + ((System.nanoTime() -
+		// deb) / 1000000000) + " secondes");
+		// deb = System.nanoTime();
+		// System.out.println("Valeur de (10,7,5,3) : " + getValue_naif(10, 7,
+		// 5, 3));
+		// System.out
+		// .println("Le temps que (10, 7, 5, 3) a prit " + ((System.nanoTime() -
+		// deb) / 1000000000) + " secondes");
+		//
+		// System.out.println("****************Algorithme
+		// dynamique****************");
+		// System.out.println("Valeur de (3,2,2,0) : " + getValue_dynamique(3,
+		// 2, 2, 0));
+		// System.out.println("Valeur de (3,1,2,0) : " + getValue_dynamique(3,
+		// 1, 2, 0));
+		// System.out.println("Valeur de (2,2,1,0) : " + getValue_dynamique(2,
+		// 2, 1, 0));
+		// System.out.println("Valeur de (2,1,1,0) : " + getValue_dynamique(2,
+		// 1, 1, 0));
+		// System.out.println("Valeur de (1,2,0,1) : " + getValue_dynamique(1,
+		// 2, 0, 1));
+		// System.out.println("Valeur de (1,1,0,0) : " + getValue_dynamique(1,
+		// 1, 0, 0));
+		// deb = System.nanoTime();
+		// System.out.println("Valeur de (10,7,7,3) : " + getValue_dynamique(10,
+		// 7, 7, 3));
+		// System.out
+		// .println("Le temps que (10, 7, 7, 3) a prit " + ((System.nanoTime() -
+		// deb) / 1000000000) + " secondes");
+		// deb = System.nanoTime();
+		// System.out.println("Valeur de (10,7,5,3) : " + getValue_dynamique(10,
+		// 7, 5, 3));
+		// System.out
+		// .println("Le temps que (10, 7, 5, 3) a prit " + ((System.nanoTime() -
+		// deb) / 1000000000) + " secondes");
+		// deb = System.nanoTime();
+		// System.out.println("Valeur de (100,100,50,50) : " +
+		// getValue_dynamique(100, 100, 50, 50));
+		// System.out.println(
+		// "Le temps que (100, 100, 50, 50) a prit " + ((System.nanoTime() -
+		// deb) / 1000000000) + " secondes");
+		// deb = System.nanoTime();
+		// System.out.println("Valeur de (100,100,48,52) : " +
+		// getValue_dynamique(100, 100, 48, 52));
+		// System.out.println(
+		// "Le temps que (100, 100, 48, 52) a prit " + ((System.nanoTime() -
+		// deb) / 1000000000) + " secondes");
+		// deb = System.nanoTime();
+		// List<Point> points = getPossiblePosition(127, 127, 127);
+		// for (Point result : points) {
+		// System.out.println("L'un des points possible qui a la valeur 127 est
+		// le suivant : (" + result.getX() + ", "
+		// + result.getY() + ")");
+		// }
+		// System.out.println(
+		// "Le temps que getPossiblePosition a prit " + ((System.nanoTime() -
+		// deb) / 1000000000) + " secondes");
+		// System.out.println("Le temps que l'ensemble du programme a prit " +
+		// ((System.nanoTime() - tot) / 1000000000)
+		// + " secondes");
+		// System.out.println("Fin du programme");
 
 		// Décommenter pour tester le jeu
 
-		int m = Integer.parseInt(args[0]);
-		int n = Integer.parseInt(args[1]);
-		int i = Integer.parseInt(args[2]);
-		int j = Integer.parseInt(args[3]);
-		game(m, n, i, j);
+		// int m = Integer.parseInt(args[0]);
+		// int n = Integer.parseInt(args[1]);
+		// int i = Integer.parseInt(args[2]);
+		// int j = Integer.parseInt(args[3]);
+		// game(m, n, i, j);
 
 	}
 
